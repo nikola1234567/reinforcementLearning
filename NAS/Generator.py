e1f89f46d7b697628bb5221325ecf56693fc5251
Jana Gjelevska <jana.gelevska@hotmail.com>
2022-02-09 01:35:29 +0100

    Generator pytorch model implemented

--- NAS/Generator.py
+++ NAS/Generator.py
@@ -1,32 +1,35 @@
+from keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
 from torch import nn
+
 
 class Generator:
     def __init__(self):
         super().__init__()
 
     def model(self, state):
-        # state num_classes, hidden_size,num_layers, learning_rate, activation_function
+        """:param state - object from class State
+         :returns sequential model"""
 
         layers = []
+        num_layers = state.num_layers
+        num_classes = state.num_classes
+        hidden_size = state.hidden_size
         for layer in range(num_layers):
             layers.append(nn.Linear(hidden_size, num_classes))
             layers.append(nn.Softmax)
         layers.append(nn.Dropout(0.25))
         return nn.Sequential(*layers)
 
-    # def model_keras(self):
-    #     model = Sequential()
-    #     model.add(Reshape((self.input), input_shape=(self.state_size,)))
-    #
-    #     model.add(Flatten())
-    #     model.add(Dense(64, activation='relu', init='he_uniform'))
-    #     model.add(Dense(32, activation='relu', init='he_uniform'))
-    #     model.add(Dense(self.action_size, activation='softmax'))
-    #     opt = Adam(lr=self.learning_rate)
-    #     model.compile(loss='categorical_crossentropy', optimizer=opt)
+    def model_keras(self, state):
+        """:param state - object from class State
+        :returns sequential keras model"""
+        num_layers = state.num_layers
+        num_classes = state.num_classes
+        hidden_size = state.hidden_size
+        model = Sequential()
+        for layer in range(num_layers):
+            model.add()
 
-
-
-
-
-
+        opt = Adam(lr=self.learning_rate)
+        return model.compile(loss='categorical_crossentropy', optimizer=opt)
